f1673d5df49334c01c67d00e5a3a7615fc64fc31
kai24112k <76855593+vmc2411@users.noreply.github.com>
2022-03-16 23:40:25 +0700

    cau hinh server

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const authRouter = require('./routes/auth')
+const postRouter = require('./routes/post')
+
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+      await mongoose.connect(   
+        process.env.MONGO_URL,
+        async (err) => {
+          if (err) throw err;
+          console.log("MongooseDB connected!");
+        }
+      );
+    } catch (error) {
+      console.log(error.message);
+      process.exit(1);
+    }
+  };
+  connectDB();
+
+  //middleware
+  app.use(express.json());
+
+
+  app.use('/api/auth',authRouter)
+  app.use('/api/posts',postRouter)
+
+app.listen(8800,() =>{
+    console.log("Backend server is running!")
+})
---
+++ server/models/Post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+const PostSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      max: 500,
+    },
+    url: {
+      type: String,
+    },
+      img: {
+        type: String,
+      },
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("posts", PostSchema);
